755270b044efff4a52280b94f758c37f5c764f4f
Thiago Raphael Raheem Marinho <thiago.raheem@gmail.com>
2025-08-14 13:03:43 -0400

    refactor: log errors instead of throwing

--- server/index.ts
+++ server/index.ts
@@ -66,12 +66,17 @@ app.use((req, res, next) => {
 (async () => {
   const server = await registerRoutes(app);
 
-  app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
+    log(err?.stack || err?.message || String(err), "error");
+
+    if (res.headersSent) {
+      return next(err);
+    }
+
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
 
     res.status(status).json({ message });
-    throw err;
   });
 
   // importantly only setup vite in development and after
