95294c8dadd8644909c1afae80f6fc56b62eabcc
Thiago Raphael Raheem Marinho <thiago.raheem@gmail.com>
2025-08-14 12:15:47 -0400

    Use environment variables for SQL Server configuration

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,8 @@
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
-dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
